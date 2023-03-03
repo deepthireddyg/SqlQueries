41da7d61561cbf660e0f73e7be0a9dc9335959ce
Deepthi <deepthireddyg@gmail.com>
2023-03-03 14:56:49 -0500

    query

--- src/RightJoin.java
+++ src/RightJoin.java
@@ -1,3 +1,7 @@
 public class RightJoin {
     //select e.emp_name,d.dept_name from employee e Right Join department d on e.dept_id=d.dept_id;
+
+    //fetch details of all emp,their manager,their department.
+    //select e.emp_name,d.dept_name,m.manager_name from employee e left join department d on e.dept_id=d.dept_id
+    //inner join manager m on m.manager_id=e.manager_id;
 }
