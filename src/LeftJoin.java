23594cd477f14cea474be5e5b74dd3403f00451c
Deepthi <deepthireddyg@gmail.com>
2023-03-03 14:48:26 -0500

    query

--- src/LeftJoin.java
+++ src/LeftJoin.java
@@ -1,4 +1,6 @@
 public class LeftJoin {
     // select e.emp_name,d.dept_name from employee e Left Join department d on e.dept_id=d.dept_id;
     // select e.emp_name,d.dept_name from department d Left Join employee e on d.dept_id=e.dept_id;
+    //fetch all the employee names
+    //select e.emp_name ,d.dept_name from employee e Left Join department d on e.dept_id=d.dept_id;
 }
