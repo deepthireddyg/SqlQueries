83bded70acdf8196f76ac1e608c3dc89bef550c0
Deepthi <deepthireddyg@gmail.com>
2023-03-03 15:33:37 -0500

    Full outer join

---
+++ src/FullOuterJoin.java
@@ -0,0 +1,3 @@
+public class FullOuterJoin {
+    //select e.emp_name,d.dept_name from employee e full join department d on e.dept_id = d.dept_id;
+}
--- src/RightJoin.java
+++ src/RightJoin.java
@@ -4,4 +4,8 @@ public class RightJoin {
     //fetch details of all emp,their manager,their department.
     //select e.emp_name,d.dept_name,m.manager_name from employee e left join department d on e.dept_id=d.dept_id
     //inner join manager m on m.manager_id=e.manager_id;
+   // fetch details of all emp,eheir manager,their department,project name;
+    // select e.emp_name,d.dept_name ,manager_name,project_name from employee e
+    // Left Join department d on e.dept_id=d.dept_id Inner
+    // Join managers m on m.manager_id=e.manager_id Left join projects p on p.team_member_id=e.emp_id;
 }
