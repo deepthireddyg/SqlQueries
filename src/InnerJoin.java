15cdaa39bca95f8d138c1ab9d80022730d7e5d4b
Deepthi <deepthireddyg@gmail.com>
2023-03-03 14:29:11 -0500

    Inner Join

---
+++ src/InnerJoin.java
@@ -0,0 +1,5 @@
+public class InnerJoin {
+    // fetch the Employee name department name that belong to
+    //select e.emp_name,d.dept_name from employee e Inner Join department d ON e.dept_id=d.dept_id;
+
+}
