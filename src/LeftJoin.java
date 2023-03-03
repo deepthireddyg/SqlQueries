0050867fa570f165a52aea458a24bebb8ba19c51
Deepthi <deepthireddyg@gmail.com>
2023-03-03 14:41:15 -0500

    RightJoin

--- src/LeftJoin.java
+++ src/LeftJoin.java
@@ -1,3 +1,4 @@
 public class LeftJoin {
     // select e.emp_name,d.dept_name from employee e Left Join department d on e.dept_id=d.dept_id;
+    // select e.emp_name,d.dept_name from department d Left Join employee e on d.dept_id=e.dept_id;
 }
---
+++ src/RightJoin.java
@@ -0,0 +1,3 @@
+public class RightJoin {
+    //select e.emp_name,d.dept_name from employee e Right Join department d on e.dept_id=d.dept_id;
+}
